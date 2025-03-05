e24ba3eacac14f0510d5083fd25d637cab0dceb9
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-05 11:37:20 +0530

    [MANVI GROVER] ADD: Calculated status by checking if number is positive negative or zero..

---
+++ CheckNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CheckNumber{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt(); 
+        
+        // Print the result
+        System.out.println("Number status: " + status(number));
+    }
+	 // Method to calculate the number status
+    public static int status(int number) {
+        if(number > 0) return 1; // Return 1 if the number is positive
+        else if(number < 0) return -1; // Return -1 if the number is negative
+        return 0; // Return 0 if the number is zero
+    }
+}
